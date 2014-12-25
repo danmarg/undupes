7c4ad3e73bebfe1eea1f1bad337133142086e9af
Daniel Margolis <dan@af0.net>
2014-12-25 01:31:07 +0100

    Improve layout. Efficient search.

--- main.go
+++ main.go
@@ -14,6 +14,11 @@ import (
 	"strings"
 )
 
+type filesWithHashes struct {
+	Unhashed string
+	Hashes   map[[md5.Size]byte][]string
+}
+
 type info struct {
 	Size  uint64
 	Names []string
@@ -46,6 +51,30 @@ func getInput(prompt string, validator func(string) bool) (string, error) {
 	return val, nil
 }
 
+func hash(path string) ([md5.Size]byte, error) {
+	var sum [md5.Size]byte
+	f, err := os.Open(path)
+	if err != nil {
+		log.Print(err)
+		return sum, err
+	}
+	defer f.Close()
+	h := md5.New()
+	_, err = io.Copy(h, f)
+	if err != nil {
+		log.Print(err)
+		return sum, err
+	}
+	s := h.Sum(nil)
+	if len(s) != len(sum) {
+		panic("Unexpected checksum length")
+	}
+	for i, v := range s {
+		sum[i] = v
+	}
+	return sum, nil
+}
+
 func main() {
 	// Get parent dir.
 	root, err := getInput("Enter parent directory to scan for duplicates in: ", func(f string) bool {
@@ -63,7 +92,7 @@ func main() {
 		root = fmt.Sprintf("%s%c", root, os.PathSeparator)
 	}
 	// Get files.
-	files := make(map[uint64]map[[md5.Size]byte][]string) // Size -> ( hash -> [name] )
+	files := make(map[uint64]*filesWithHashes)
 	ps := []rune{'|', '/', '-', '\\', '|', '/', '-', '\\'}
 	i := 0
 	fmt.Printf("Indexing %s \n \r", root)
@@ -72,46 +101,48 @@ func main() {
 			return nil
 		}
 		i++
-		fmt.Printf("%c \r", ps[(i%100)%len(ps)])
+		fmt.Printf("%c \r", ps[(int(i/30))%len(ps)])
 		hs, ok := files[uint64(info.Size())]
 		if !ok {
-			hs = make(map[[md5.Size]byte][]string)
-			files[uint64(info.Size())] = hs
+			// If we've never seen another file of this size, we don't have to do the md5 sum.
+			files[uint64(info.Size())] = &filesWithHashes{
+				Unhashed: path,
+				Hashes:   make(map[[md5.Size]byte][]string),
+			}
+			return nil
 		}
-		f, err := os.Open(path)
+		// Add the hash of this file.
+		sum, err := hash(path)
 		if err != nil {
-			log.Print(err)
 			return err
 		}
-		defer f.Close()
-		h := md5.New()
-		_, err = io.Copy(h, f)
-		if err != nil {
-			log.Print(err)
-			return err
-		}
-		var sum [md5.Size]byte
-		s := h.Sum(nil)
-		if len(s) != len(sum) {
-			panic("Unexpected checksum length")
-		}
-		for i, v := range s {
-			sum[i] = v
-		}
-		fs, ok := hs[sum]
+		fs, ok := hs.Hashes[sum]
 		if !ok {
-			hs[sum] = []string{path}
+			hs.Hashes[sum] = []string{path}
 		} else {
-			hs[sum] = append(fs, path)
+			hs.Hashes[sum] = append(fs, path)
 		}
-
+		// And check if there is also an unhashed file.
+		if hs.Unhashed != "" {
+			sum, err := hash(hs.Unhashed)
+			if err != nil {
+				return err
+			}
+			fs, ok := hs.Hashes[sum]
+			if !ok {
+				hs.Hashes[sum] = []string{hs.Unhashed}
+			} else {
+				hs.Hashes[sum] = append(fs, hs.Unhashed)
+			}
+			hs.Unhashed = ""
+		}
 		return nil
 	})
 	dupes := []info{}
 	fcount := 0
 	tsize := uint64(0)
-	for size, hashes := range files {
-		for _, files := range hashes {
+	for size, hs := range files {
+		for _, files := range hs.Hashes {
 			if len(files) > 1 {
 				dupes = append(dupes, info{Size: size, Names: files})
 				fcount += len(files)
@@ -125,11 +156,15 @@ func main() {
 
 	sort.Sort(sort.Reverse(bySize(dupes)))
 
-	fmt.Printf("\nReviewing results: for each duplicate fileset, select 'f' to delete all but the first file, 'a' to keep all files, or 'n' followed by a file number (e.g. n2) to delete all except the second file.\n")
+	fmt.Printf("\nReviewing results:\nFor each duplicate fileset, select 'f' to delete all but the first file, 'a' to keep all files, or 'n' (e.g. 2) to delete all except the second file.\n")
 
 	for _, dupe := range dupes {
 		keep := -1
-		_, err := getInput(fmt.Sprintf("%s: %v\nKeep [F]irst/[a]ll/[n]th? ", humanize.Bytes(dupe.Size*uint64(len(dupe.Names)-1)), dupe.Names), func(v string) bool {
+		names := ""
+		for i, n := range dupe.Names {
+			names += fmt.Sprintf("%d: %s\n", i+1, n)
+		}
+		_, err := getInput(fmt.Sprintf("%s:\n%s\nKeep [F]irst/[a]ll/[n]th? ", humanize.Bytes(dupe.Size*uint64(len(dupe.Names)-1)), names), func(v string) bool {
 			switch strings.ToLower(v) {
 			case "f":
 				keep = 0
@@ -137,21 +172,16 @@ func main() {
 			case "a":
 				return true
 			default:
-				if strings.HasPrefix(strings.ToLower(v), "n") {
-					k, err := strconv.ParseInt(v[1:], 10, 32)
-					if err != nil {
-						return false
-					}
-					if k < 1 || int(k) > len(dupe.Names) {
-						return false
-					}
-					keep = int(k) - 1
-					return true
-				} else {
+				k, err := strconv.ParseInt(v, 10, 32)
+				if err != nil {
 					return false
 				}
+				if k < 1 || int(k) > len(dupe.Names) {
+					return false
+				}
+				keep = int(k) - 1
+				return true
 			}
-			return false
 		})
 		if err != nil {
 			log.Fatal(err)
