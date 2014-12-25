7e5336830c3fa66ee599b50277a2ebe10e14ba4d
Daniel Margolis <dan@af0.net>
2014-12-25 02:20:05 +0100

    Refactor into two files.

--- main.go
+++ main.go
@@ -2,35 +2,13 @@ package main
 
 import (
 	"bufio"
-	md5 "crypto/md5"
 	"fmt"
 	"github.com/dustin/go-humanize"
-	"io"
 	"log"
 	"os"
-	filepath "path/filepath"
-	"sort"
 	"strconv"
 	"strings"
 )
-
-type filesWithHashes struct {
-	Unhashed string
-	Hashes   map[[md5.Size]byte][]string
-}
-
-type info struct {
-	Size  uint64
-	Names []string
-}
-
-type bySize []info
-
-func (a bySize) Len() int      { return len(a) }
-func (a bySize) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
-func (a bySize) Less(i, j int) bool {
-	return a[i].Size*uint64(len(a[i].Names)) < a[j].Size*uint64(len(a[j].Names))
-}
 
 func getInput(prompt string, validator func(string) bool) (string, error) {
 	// Reader to read from user input.
@@ -53,30 +31,6 @@ func getInput(prompt string, validator func(string) bool) (string, error) {
 	return val, nil
 }
 
-func hash(path string) ([md5.Size]byte, error) {
-	var sum [md5.Size]byte
-	f, err := os.Open(path)
-	if err != nil {
-		log.Print(err)
-		return sum, err
-	}
-	defer f.Close()
-	h := md5.New()
-	_, err = io.Copy(h, f)
-	if err != nil {
-		log.Print(err)
-		return sum, err
-	}
-	s := h.Sum(nil)
-	if len(s) != len(sum) {
-		panic("Unexpected checksum length")
-	}
-	for i, v := range s {
-		sum[i] = v
-	}
-	return sum, nil
-}
-
 func main() {
 	// Get parent dir.
 	root, err := getInput("Enter parent directory to scan for duplicates in: ", func(f string) bool {
@@ -93,70 +47,20 @@ func main() {
 	if !os.IsPathSeparator(root[len(root)-1]) {
 		root = fmt.Sprintf("%s%c", root, os.PathSeparator)
 	}
-	// Get files.
-	files := make(map[uint64]*filesWithHashes)
-	ps := []rune{'|', '/', '-', '\\', '|', '/', '-', '\\'}
-	i := 0
-	fmt.Printf("Indexing %s \n \r", root)
-	filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-		if info.IsDir() {
-			return nil
-		}
-		i++
-		fmt.Printf("%c \r", ps[(int(i/30))%len(ps)])
-		hs, ok := files[uint64(info.Size())]
-		if !ok {
-			// If we've never seen another file of this size, we don't have to do the md5 sum.
-			files[uint64(info.Size())] = &filesWithHashes{
-				Unhashed: path,
-				Hashes:   make(map[[md5.Size]byte][]string),
-			}
-			return nil
-		}
-		// Add the hash of this file.
-		sum, err := hash(path)
-		if err != nil {
-			return err
-		}
-		fs, ok := hs.Hashes[sum]
-		if !ok {
-			hs.Hashes[sum] = []string{path}
-		} else {
-			hs.Hashes[sum] = append(fs, path)
-		}
-		// And check if there is also an unhashed file.
-		if hs.Unhashed != "" {
-			sum, err := hash(hs.Unhashed)
-			if err != nil {
-				return err
-			}
-			fs, ok := hs.Hashes[sum]
-			if !ok {
-				hs.Hashes[sum] = []string{hs.Unhashed}
-			} else {
-				hs.Hashes[sum] = append(fs, hs.Unhashed)
-			}
-			hs.Unhashed = ""
-		}
-		return nil
-	})
-	dupes := []info{}
+	dupes, err := Dupes(root)
+	if err != nil {
+		log.Fatal(err)
+	}
 	fcount := 0
 	tsize := uint64(0)
-	for size, hs := range files {
-		for _, files := range hs.Hashes {
-			if len(files) > 1 {
-				dupes = append(dupes, info{Size: size, Names: files})
-				fcount += len(files)
-				tsize += size * uint64(len(files))
-			}
-		}
+	for _, i := range dupes {
+		fcount += len(i.Names)
+		tsize += i.Size * uint64(len(i.Names))
 	}
+
 	fmt.Printf("Found %d sets of duplicate files\n", len(dupes))
 	fmt.Printf("Total file count: %d\n", fcount)
 	fmt.Printf("Total size used: %s\n", humanize.Bytes(tsize))
-
-	sort.Sort(sort.Reverse(bySize(dupes)))
 
 	fmt.Printf("\nReviewing results:\nFor each duplicate fileset, select 'f' to delete all but the first file, 'a' to keep all files, or 'n' (e.g. 2) to delete all except the second file.\n")
 
